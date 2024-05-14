c90c43b3bbf365b0a1ba73f3382cd4e35c2b04ac
Rubinart <k.ivanov19981@gmail.com>
2024-05-14 14:25:11 +0300

    added schemas

--- src/app/sql_app/schemas.py
+++ src/app/sql_app/schemas.py
@@ -1,25 +1,59 @@
+from typing import List
 from pydantic import BaseModel
+from .models import Card, Transaction
 
 
-class ItemBase(BaseModel):
-    title: str
-    description: str | None = None
+class CardBase(BaseModel):
+    number: str
+    card_holder: str
+    exp_date: str
+    cvv: str
+    design: str
 
 
-class ItemCreate(ItemBase):
+class CardCreate(CardBase):
     pass
 
 
-class Item(ItemBase):
+class Card(CardBase):
     id: int
-    owner_id: int
+    user_id: int
 
     class Config:
         from_attributes = True
 
 
+class TransactionBase(BaseModel):
+    id: int
+    amount: int
+    timestamp: str
+    category: str
+    card_id: int
+    user_id: int
+
+
+class TransactionCreate(Transaction):
+    pass
+
+
+class Transaction(TransactionBase):
+    id: int
+    timestamp: str
+    category: str
+    card_id: int
+    user_id: int
+    
+    class Config:
+        from_attributes = True
+
+
 class UserBase(BaseModel):
+    username: str
     email: str
+    contact_number: str
+    is_active: bool
+    is_admin: bool
+    photo: str
 
 
 class UserCreate(UserBase):
@@ -28,8 +62,9 @@ class UserCreate(UserBase):
 
 class User(UserBase):
     id: int
-    is_active: bool
-    items: list[Item] = []
+    hashed_password: str
+    cards: List[Card] = []
+    transactions: List[Transaction] = []
 
     class Config:
         from_attributes = True
