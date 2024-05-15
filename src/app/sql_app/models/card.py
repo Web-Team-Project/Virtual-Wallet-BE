8a3bec0c33b968049d7786d4817d7b38151b4ca1
Rubinart <k.ivanov19981@gmail.com>
2024-05-15 16:56:50 +0300

    card models

---
+++ src/app/sql_app/models/card.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
+from sqlalchemy.orm import relationship
+from app.sql_app.database import Base
+
+
+class Card(Base):
+    __tablename__ = "cards"
+
+    id = Column(Integer, primary_key=True, index=True)
+    number = Column(String, unique=True, index=True)
+    card_holder = Column(String)
+    exp_date = Column(DateTime)
+    cvv = Column(String)
+    design = Column(String) # Image url
+    user_id = Column(Integer, ForeignKey("users.id"))
+    user = relationship("User", back_populates="cards")
--- src/app/sql_app/models/user.py
+++ src/app/sql_app/models/user.py
@@ -6,11 +6,11 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
-    sub = Column(String, unique=True)
+    sub = Column(String, unique=True, index=True)
     name = Column(String)
     given_name = Column(String)
     family_name = Column(String)
     picture = Column(String)
-    email = Column(String, unique=True)
+    email = Column(String, unique=True, index=True)
     email_verified = Column(Boolean)
     locale = Column(String)
