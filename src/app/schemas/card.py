783d196a4b7c20c41379d4dd3335bef01107092e
Rubinart <k.ivanov19981@gmail.com>
2024-05-15 22:15:12 +0300

    card restrictions

--- src/app/schemas/card.py
+++ src/app/schemas/card.py
@@ -1,12 +1,12 @@
 from datetime import datetime
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
 class CardBase(BaseModel):
-    number: str
-    card_holder: str
+    number: str = Field(..., min_length=16, max_length=16)
+    card_holder: str = Field(..., min_length=2, max_length=30)
     exp_date: datetime
-    cvv: str
+    cvv: str = Field(..., min_length=3, max_length=3)
     design: str
     user_id: int
 
--- src/app/services/crud/user.py
+++ src/app/services/crud/user.py
@@ -1,9 +1,6 @@
 from fastapi import HTTPException, status
-from app.schemas.user import UserCreate
 from app.sql_app.models.user import User
 from starlette.requests import Request
-from sqlalchemy.orm import Session
-from sql_app.models import User
 
 
 async def get_current_user(request: Request) -> User:
@@ -12,15 +9,3 @@ async def get_current_user(request: Request) -> User:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, 
                             detail="User not authenticated.")
     return User(**user)
-
-
-async def get_user(db: Session, user_id: int):
-    return db.query(User).filter(User.id == user_id).first()
-
-
-async def update_user(db: Session, user_id: int, user: UserCreate):
-    user_db = await get_user(db, user_id)
-    user_data = dict(user.__dict__)
-    db.query(User).filter(User.id == user_id).update(user_data)
-    db.commit()
-    return user_db
