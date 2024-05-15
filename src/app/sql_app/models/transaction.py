fbd4f460c650d98065b37420d25dbc50395bcb99
Rubinart <k.ivanov19981@gmail.com>
2024-05-15 17:13:39 +0300

    transaction models

---
+++ src/app/sql_app/models/transaction.py
@@ -0,0 +1,16 @@
+from app.sql_app.database import Base
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
+from sqlalchemy.orm import relationship
+
+
+class Transaction(Base):
+    __tablename__ = "transactions"
+
+    id = Column(Integer, primary_key=True, index=True)
+    amount = Column(Integer)
+    timestamp = Column(DateTime)
+    category = Column(String)
+    card_id = Column(Integer, ForeignKey("cards.id"))
+    user_id = Column(Integer, ForeignKey("users.id"))
+    card = relationship("Card", back_populates="transactions")
+    user = relationship("User", back_populates="transactions")
