85b5d061ab5d74064e01245d5194a7add4d9975d
Rubinart <k.ivanov19981@gmail.com>
2024-05-16 15:29:44 +0300

    card endpoints

--- src/app/api/api_v1/endpoints/card.py
+++ src/app/api/api_v1/endpoints/card.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import Session
 from app.services.common.utils import process_request
 from app.services.crud.user import get_current_user
 from app.sql_app.database import get_db
-from app.services.crud.card import create_card
+from app.services.crud.card import create_card, read_card, update_card, delete_card
 from app.sql_app.models.models import User, Card
 
 
@@ -22,16 +22,27 @@ async def create(card: CardCreate, db: Session = Depends(get_db), current_user: 
 
 
 @router.get("/cards/{card_id}")
-async def get_card(card_id: int):
-    pass
+async def read(card_id: int):
+    
+    async def _read_card():
+        return await read_card(card_id)
+
+    return await process_request(_read_card)
 
 
 @router.put("/cards/{card_id}")
-async def update_card(card_id: int):
-    pass
+async def update(card_id: int, card: CardCreate, db: Session = Depends(get_db), current_user: User = Depends(get_current_user)):
+    
+    async def _update_card():
+        return await update_card(db, card_id, card, current_user.id)
+
+    return await process_request(_update_card)
 
 
 @router.delete("/cards/{card_id}")
-async def delete_card(card_id: int, db: Session = Depends(get_db), current_user: User = Depends(get_current_user)):
-    await _delete_card(db, card_id, current_user.id)
-    return {"message": "Card deleted successfully."}
+async def delete(card_id: int, db: Session = Depends(get_db), current_user: User = Depends(get_current_user)):
+
+    async def _delete_card():
+        return await delete_card(db, card_id, current_user.id)
+
+    return await process_request(_delete_card)
--- src/app/services/crud/card.py
+++ src/app/services/crud/card.py
@@ -17,6 +17,29 @@ async def create_card(db: Session, card: CardCreate, user_id: int):
     return db_card
 
 
+async def read_card(db: Session, card_id: int):
+    db_card = db.query(Card).filter(Card.id == card_id).first()
+    if db_card is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
+                            detail="Card not found.")
+    return db_card
+
+
+async def update_card(db: Session, card_id: int, card: CardCreate, user_id: int):
+    db_card = db.query(Card).filter(Card.id == card_id, Card.user_id == user_id).first()
+    if not db_card:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
+                            detail="Card not found.")
+    db_card.number = card.number
+    db_card.card_holder = card.card_holder
+    db_card.exp_date = card.exp_date
+    db_card.cvv = card.cvv
+    db_card.design = card.design
+    db.commit()
+    db.refresh(db_card)
+    return db_card
+
+
 async def delete_card(db: Session, card_id: int, user_id: int):
     db_card = db.query(Card).filter(Card.id == card_id, Card.user_id == user_id).first()
     if db_card is None:
