70a35ae750975c493a7fa1daa5807a0bb183d011
Rubinart <k.ivanov19981@gmail.com>
2024-05-17 18:13:18 +0300

    contact schema

---
+++ src/app/schemas/contact.py
@@ -0,0 +1,17 @@
+from pydantic import BaseModel
+from uuid import UUID
+
+class ContactBase(BaseModel):
+    user_id: UUID
+    user_contact_id: UUID
+
+
+class ContactCreate(ContactBase):
+    pass
+
+
+class Contact(ContactBase):
+    id: UUID
+
+    class Config:
+        from_attributes = True
