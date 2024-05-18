7841b934aee11e3e2dbd656396c61ecdfe35dfbc
Rubinart <k.ivanov19981@gmail.com>
2024-05-18 08:55:51 +0300

    updated models

--- src/app/sql_app/models/models.py
+++ src/app/sql_app/models/models.py
@@ -2,7 +2,7 @@ from sqlalchemy import Date, create_engine
 from sqlalchemy import Column, DateTime, ForeignKey, Integer, String, Boolean, Enum
 from sqlalchemy.orm import relationship
 from app.sql_app.database import Base
-from app.sql_app.models.role import Role
+from app.sql_app.models.enum import Role, Status
 from sqlalchemy.dialects.postgresql import UUID
 import uuid
 
@@ -34,7 +34,7 @@ class Card(Base):
     exp_date = Column(Date)
     cvv = Column(String)
     design = Column(String) # Image url
-    user_id = Column(UUID(as_uuid=True), ForeignKey("users.id"))
+    user_id = Column(UUID(as_uuid=True), ForeignKey("users.id"), nullable=False)
 
     user = relationship("User", back_populates="cards")
     transactions = relationship("Transaction", back_populates="card")
@@ -44,16 +44,19 @@ class Transaction(Base):
     __tablename__ = "transactions"
 
     id = Column(UUID(as_uuid=True), primary_key=True, index=True, default=uuid.uuid4, unique=True, nullable=False)
-    amount = Column(Integer)
+    amount = Column(Integer) # Amount might be switched to float
     timestamp = Column(DateTime(timezone=True))
     category = Column(String)
-    is_recurring = Column(Boolean)
-    card_id = Column(UUID(as_uuid=True), ForeignKey("cards.id"))
-    user_id = Column(UUID(as_uuid=True), ForeignKey("users.id"))
-    category_id = Column(UUID(as_uuid=True), ForeignKey("categories.id"))
+    is_recurring = Column(Boolean, default=False)
+    status = Column(Enum(Status), default="pending")
+    card_id = Column(UUID(as_uuid=True), ForeignKey("cards.id"), nullable=False)
+    sender_id = Column(UUID(as_uuid=True), ForeignKey("users.id"), nullable=False)
+    recipient_id = Column(UUID(as_uuid=True), ForeignKey("users.id"), nullable=False)
+    category_id = Column(UUID(as_uuid=True), ForeignKey("categories.id"), nullable=False)
 
     card = relationship("Card", back_populates="transactions")
-    user = relationship("User", back_populates="transactions")
+    sender = relationship("User", back_populates="transactions", foreign_keys=[sender_id])
+    recipient = relationship("User", back_populates="transactions", foreign_keys=[recipient_id])
     category = relationship("Category", back_populates="transactions")
 
 
@@ -70,11 +73,25 @@ class Contact(Base):
     __tablename__ = "contacts"
 
     id = Column(UUID(as_uuid=True), primary_key=True, index=True, default=uuid.uuid4, unique=True, nullable=False)
-    user_id = Column(UUID(as_uuid=True), ForeignKey("users.id"))
-    user_contact_id = Column(UUID(as_uuid=True), ForeignKey("users.id"))
+    user_id = Column(UUID(as_uuid=True), ForeignKey("users.id"), nullable=False)
+    user_contact_id = Column(UUID(as_uuid=True), ForeignKey("users.id"), nullable=False)
 
     user = relationship("User", back_populates="contacts", foreign_keys=[user_id])
     user_contact = relationship("User", back_populates="contacts", foreign_keys=[user_contact_id])
+
+
+class RecurringTransaction(Base):
+    __tablename__ = "recurring_transactions"
+
+    id = Column(UUID(as_uuid=True), primary_key=True, index=True, default=uuid.uuid4, unique=True, nullable=False)
+    user_id = Column(UUID(as_uuid=True), ForeignKey("users.id"), nullable=False)
+    card_id = Column(UUID(as_uuid=True), ForeignKey("cards.id"), nullable=False)
+    amount = Column(Integer, nullable=False) # Amount might be switched to float
+    interval = Column(String, nullable=False)
+    next_execution_date = Column(Date, nullable=False)
+
+    user = relationship("User")
+    card = relationship("Card")
 
 
 SYNC_DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/virtual-wallet-db"
