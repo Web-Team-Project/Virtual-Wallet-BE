fcfc18e423549961cb04b556ba20b1e809a82a9e
Rubinart <k.ivanov19981@gmail.com>
2024-05-24 16:08:32 +0300

    removed asyncio run

--- src/app/sql_app/models/models.py
+++ src/app/sql_app/models/models.py
@@ -134,5 +134,3 @@ settings = get_settings()
 async def create_tables():
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
-
-asyncio.run(create_tables())
