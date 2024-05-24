82d2c49e02495dd260542bce154c9ceb6731ef51
Rubinart <k.ivanov19981@gmail.com>
2024-05-24 16:29:07 +0300

    changed localhost

--- src/run_server.py
+++ src/run_server.py
@@ -24,7 +24,7 @@ if __name__ == "__main__":
         "--port",
         type=int,
         default=8000,
-        help="port to listen on (default: 8080)",
+        help="port to listen on (default: 8000)",
     )
     config = parser.parse_args()
 
@@ -33,7 +33,7 @@ if __name__ == "__main__":
 
     uvicorn.run(
         "app.main:app",
-        host="localhost",
+        host="0.0.0.0",
         port=config.port,
         reload=reload_enabled,
         reload_dirs=reload_dirs,
