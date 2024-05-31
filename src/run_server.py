9d7acd74936e5e3160147a7082e0195d2d675b82
Rubinart <k.ivanov19981@gmail.com>
2024-05-31 20:52:29 +0300

    changes to dockerfile

--- src/run_server.py
+++ src/run_server.py
@@ -33,7 +33,7 @@ if __name__ == "__main__":
 
     uvicorn.run(
         "app.main:app",
-        host="0.0.0.0",
+        host="localhost",
         port=config.port,
         reload=reload_enabled,
         reload_dirs=reload_dirs,
